cd319fa4fd150d36b17bf3407edc691f23de3591
Pratyaoy Sarkar <sarkarpratyaoy@gmail.com>
2025-06-02 22:49:57 +0530

    Added Code to reverse a sum by UDP

--- TCP_ReverseSum.c
+++ TCP_ReverseSum.c
@@ -24,6 +24,7 @@ listenfd = socket(AF_INET, SOCK_STREAM, 0);
 bzero(&servaddr, sizeof(servaddr));
 servaddr.sin_family = AF_INET;
 servaddr.sin_port = htons(SERV_PORT);
+servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 blind(listenfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
 listen(listenfd, 1);
